adf47e0e677cd96e0680714e3b3f7a828c11f550
Igor Rotondo Bagliotti <igorbag@hotmail.com>
2021-05-05 22:12:19 -0300

    Adicionando menus da aula 2

--- app/src/main/java/com/example/santanderdevweek/MainActivity.kt
+++ app/src/main/java/com/example/santanderdevweek/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.santanderdevweek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_1 -> {
+                Log.d("CLICK", "Click no item 1")
+                true
+            } else -> super.onOptionsItemSelected(item)
+        }
     }
 }
